33386b2fab78885fafb88ced46170c0d8a2252fe
tatukristiani <tatu.pulkkinen@metropolia.fi>
2024-01-08 02:06:19 +0200

    Removed unncesseccary stuff

--- src/utility/UserContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from "react";
-
-/**
- * Used for sharing user information across the application.
- * @type {React.Context<null>}
- */
-export const UserContext = createContext(null);
--- src/utility/UserMoviesContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from "react";
-
-/**
- * Used to share logged users movies accross the application.
- * @type {React.Context<null>}
- */
-export const UserMoviesContext = createContext(null);
