df434189a460ccc6805de4d418d389b93194590a
tatukristiani <tatu.pulkkinen@metropolia.fi>
2024-06-19 21:07:37 +0300

    made some visual improvements

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Movie from "./Movie";
 import { useParams, useNavigate } from "react-router-dom";
-import '../styles/Movies.css';
+import "../styles/Movies.css";
 import Paginate from "./Paginate";
-import MyMoviesAPI from '../api/MyMoviesAPI';
-import axios from '../api/axios';
+import MyMoviesAPI from "../api/MyMoviesAPI";
+import axios from "../api/axios";
+import { GridLoader } from "react-spinners";
+
 /**
  * List of movies to be shown at page. This is mostly used all around but works only when searching with genre.
  * Need the code param from the url to get succeeded results.
@@ -13,44 +15,74 @@ import axios from '../api/axios';
  * @constructor Creates the Movies component.
  */
 const Movies = () => {
-    const [movies, setMovies] = useState([]);
-    const { code } = useParams();
-    const { page } = useParams();
-    const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [moviesLoaded, setMoviesLoaded] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const { code } = useParams();
+  const { page } = useParams();
+  const navigate = useNavigate();
 
-    const handlePageClick = (data) => {
-        navigate(`/movies/${code}/${data.selected + 1}`);
-        window.scrollTo({ top: 0 });
+  const handlePageClick = (data) => {
+    navigate(`/movies/${code}/${data.selected + 1}`);
+    window.scrollTo({ top: 0 });
+  };
+
+  const fetchMovies = async () => {
+    const url =
+      code === "trending"
+        ? MyMoviesAPI.getTrendingURL(page)
+        : MyMoviesAPI.getGenreURL(code, page);
+    await axios.get(url).then((res) => {
+      setMovies(res.data);
+      setMoviesLoaded(true);
+    });
+  };
+
+  const uniqueId = (prefix = "id-") =>
+    prefix + Math.random().toString(16).slice(-4);
+
+  useEffect(() => {
+    setLoading(true);
+    setMoviesLoaded(false);
+    fetchMovies();
+  }, [code, page]);
+
+  useEffect(() => {
+    if (moviesLoaded) {
+      setLoading(false);
     }
+  }, [moviesLoaded]);
 
-    const fetchMovies = async () => {
-        const url = code === "trending" ? MyMoviesAPI.getTrendingURL(page) : MyMoviesAPI.getGenreURL(code, page);
-        await axios.get(url).then(res => {
-            setMovies(res.data);
-        })
-
-    }
-
-    const uniqueId = (prefix = 'id-') => prefix + Math.random().toString(16).slice(-4);
-
-    useEffect(() => {
-        fetchMovies();
-    }, [code, page])
-
-    return (
-        <>
-            <div>
-                <div className="movies-container">
-                    {movies.map((movie => (
-                        <Movie key={uniqueId(movie.id)} movie={movie} databaseData={false} />
-                    )))}
-                </div>
-                <div className='paginate-container'>
-                    <Paginate onPageChange={handlePageClick} currentPage={page} />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      {loading ? (
+        <div className="loader">
+          <GridLoader
+            color="white"
+            loading={true}
+            size={10}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          <div className="movies-container">
+            {movies.map((movie) => (
+              <Movie
+                key={uniqueId(movie.id)}
+                movie={movie}
+                databaseData={false}
+              />
+            ))}
+          </div>
+          <div className="paginate-container">
+            <Paginate onPageChange={handlePageClick} currentPage={page} />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Movies;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import Movie from "./Movie";
 import axios from "../api/axios";
-import '../styles/Search.css';
-import MyMoviesAPI from '../api/MyMoviesAPI';
+import "../styles/Search.css";
+import MyMoviesAPI from "../api/MyMoviesAPI";
+import { GridLoader } from "react-spinners";
 
 /**
  * Search page which includes the SearchBar component and a container to show the results of the search.
@@ -11,49 +12,76 @@ import MyMoviesAPI from '../api/MyMoviesAPI';
  * @constructor Creates the Search component.
  */
 const Search = () => {
-    const [searchResults, setSearchResults] = useState([]);
-    const [search, setSearch] = useState('');
-    const [searched, setSearched] = useState(false);
-    const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [moviesLoaded, setMoviesLoaded] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searched, setSearched] = useState(false);
+  const [error, setError] = useState("");
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    setSearched(false);
+    setSearchResults([]);
+  };
 
-    const handleSearchChange = (e) => {
-        setSearch(e.target.value);
-        setSearched(false);
-        setSearchResults([]);
-    }
-
-    // Sends the request to search for a movie with the given input.
-    const handleSearch = async (e) => {
-        if (e.key === 'Enter' || e.target.tagName.toLowerCase() === 'i') {
-            await axios.get(MyMoviesAPI.searchMovieURL(search)).then(res => {
-                if (res.data.length >= 1) {
-                    setSearched(true);
-                    let resultsArray = res.data.filter(function (movie) {
-                        return movie.posterPath
-                    });
-                    setSearchResults(resultsArray);
-                } else {
-                    setError(res.data.message);
-                }
-            })
+  // Sends the request to search for a movie with the given input.
+  const handleSearch = async (e) => {
+    if (e.key === "Enter" || e.target.tagName.toLowerCase() === "i") {
+      setLoading(true);
+      await axios.get(MyMoviesAPI.searchMovieURL(search)).then((res) => {
+        if (res.data.length >= 1) {
+          setSearched(true);
+          let resultsArray = res.data.filter(function (movie) {
+            return movie.posterPath;
+          });
+          setSearchResults(resultsArray);
+        } else {
+          setError(res.data.message);
         }
+        setLoading(false);
+      });
     }
+  };
 
-    return (
+  useEffect(() => {
+    if (moviesLoaded) {
+      setLoading(false);
+    }
+  }, [moviesLoaded]);
+
+  return (
+    <>
+      <SearchBar
+        value={search}
+        onChange={handleSearchChange}
+        handleSearch={(e) => handleSearch(e)}
+      />
+      {loading ? (
+        <div className="loader">
+          <GridLoader
+            color="white"
+            loading={true}
+            size={10}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
         <>
-            <SearchBar value={search} onChange={handleSearchChange} handleSearch={(e) => handleSearch(e)} />
-            {error && <p className='search-error'>{error}</p>}
-            {searched &&
-                <p className='search-value'>Search Results Found with '{search}'</p>
-            }
-            <div className='search-result-container'>
-                {searchResults.map((movie => (
-                    <Movie key={movie.id} movie={movie} databaseData={false} />
-                )))}
-            </div>
+          {error && <p className="search-error">{error}</p>}
+          {searched && (
+            <p className="search-value">Search Results Found with '{search}'</p>
+          )}
+          <div className="search-result-container">
+            {searchResults.map((movie) => (
+              <Movie key={movie.id} movie={movie} databaseData={false} />
+            ))}
+          </div>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Search;
